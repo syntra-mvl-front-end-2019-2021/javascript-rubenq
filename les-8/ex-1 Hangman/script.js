cb9649cc74752faf55323ad4936dcdbf7a03f079
Ruben <rubenquaegebeur@gmail.Com>
2020-06-23 15:21:36 +0200

    Hangman

--- les-8/ex-1 Hangman/script.js
+++ les-8/ex-1 Hangman/script.js
@@ -51,9 +51,10 @@ const randomWords = [
     'prospect',
   ];
   const hangManImage = document.querySelector('#image');
-  const solutionContainer = document.querySelector('#solution-container');
+  let solutionContainer = document.querySelector('#solution-container');
   const winOrLoseContainer = document.querySelector('#win-lose-container');
   const letterContainer = document.querySelector('#letter-container');
+  const letters = document.getElementsByClassName('letter');
   const gameState = {
     word: [],
     hangman: 1,
@@ -63,18 +64,70 @@ const randomWords = [
     lost: false,
   };
   
-  function initGame() {
-    // choose a random word > save in gamestate
-    // reset gamestate
-    // update solutionContainer > insert div.solution-letter per letter
-    // empty winOrLoseContainer.innerHTML
-    // remove success and failed classes from .letter
+  function initGame() 
+  {
+    let letter = letterContainer.children;
+    gameState.word = randomWords[Math.floor(Math.random() * randomWords.length)];
+    gameState.hangman = 1;
+    gameState.turn = 1;
+    gameState.lettersFound = 0;
+    gameState.won = false;
+    gameState.lost = false;
+    // Set class to 'letter' for children inside 'letter-container'.
+      for (let i = 0; i < letter.length; i++)
+      {
+        letter[i].setAttribute('class', 'letter');
+      }
+    // Set 'hangManImage' to 'hangman01.png'.
+    hangManImage.setAttribute('src', 'images/hangman01.png');
+    // Set 'solutionContainer' to equal letters of randomWord.
+      for (let i = gameState.word.length; i > 0; i--)
+      {
+        solutionContainer.innerHTML += '<div class="solution-letter"></div>';
+      }
+    // Set 'winOrLoseContainer' to empty.
+    winOrLoseContainer.innerHTML = '';
   }
   
   function clickLetter(event) {
     // check if event.target is .letter
     // check if event.target has success or failed class
     // ---
+    checkLetter();
+    winOrLose();
+  }
+
+  function checkLetter();
+  for (let i = 0; i > word.length; i++)
+  {
+    if (letter == word[i])
+    {
+
+    }
   }
   
-  // hangManImage.setAttribute('src', 'images/hangman02.png');
+
+ letters.forEach(function(letters) {letters.addEventListener('click', clickLetter)});
+
+  function winOrLose()
+  {
+    if (gameState.lettersFound === gameState.word.length)
+      {
+        gameState.won = true;
+      }
+      else
+        {
+          gameState.won = false;
+        }
+    if (gameState.hangman === 9)
+        {
+          gameState.lost = true;
+        }
+      else
+        {
+          gameState.lost = false;
+        }
+    console.log(gameState.won);
+    
+  }
+  initGame();
