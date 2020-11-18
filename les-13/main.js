d64226981af62f80e6071da6aa85cabe2d0f81b0
Ruben <rubenquaegebeur@gmail.Com>
2020-11-18 12:16:21 +0100

    Oefening promise + slider

--- les-13/main.js
+++ les-13/main.js
@@ -1,62 +1,90 @@
-// var i = 0;
-// var images = [];
-// var time = 3000;
-	 
+// PROMISE
 
-// images[0] = "https://unsplash.com/photos/sXxwbzfNdR4";
-// images[1] = "https://unsplash.com/photos/AJO4WvlpCzM";
-// images[2] = "https://unsplash.com/photos/eTreY8QuLlU";
-// images[3] = "https://unsplash.com/photos/MsgKv2yK0uY";
-
-
-function changeImg(){
-	document.slide.src = images[i];
-	if(i < images.length - 1){
-	  i++; 
-	} else { 
-		i = 0;
-	}
-	setTimeout("changeImg()", time);
-}
-// window.onload=changeImg;
-
-
-const img1 = "images/image1.jpg";
-const img2 = "images/image2.jpg";
-const images = document.querySelectorAll('.images');
+const img1 = "https://images.unsplash.com/photo-1470116109808-c71d8bd6f4a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80";
+const img2 = "https://images.unsplash.com/photo-1470117144297-a67b448680bb?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80";
+const img3 = "https://images.unsplash.com/photo-1477044545293-98b9221de30a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80";
+const img4 = "https://images.unsplash.com/photo-1478105069489-aca3e3fedcf1?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80";
         
-        let promise1 = new Promise((resolve, reject)=>{
-            let img = document.createElement('img');
-            img.addEventListener('load', (ev) => {
-                console.log(img);
-                    resolve(img);
-                });
-            img.addEventListener('error', (err) => {
-                    console.log('Image 1 is NOT loaded');
-                    reject(err);
-                });
-            img.src = "images/image1.jpg";
+let promise1 = new Promise((resolve, reject)=>{
+    let img = document.createElement('img');
+    img.addEventListener('load', (ev) => {
+        // console.log(img);
+            resolve(img);
         });
-        
-        let promise2 = new Promise((resolve, reject)=>{
-            let img = document.createElement('img');
-            img.addEventListener('load', (ev) => {
-               
-                console.log(img);
-                    resolve(img);
-                });
-            img.addEventListener('error', (err) => {
-                    console.log('Image 2 is NOT loaded');
-                    reject(err);
-                });
-            img.src = "images/image2.jpg";
+    img.addEventListener('error', (err) => {
+            console.log('Image 1 is NOT loaded');
+            reject(err);
         });
-        
-        Promise.all([promise1, promise2]).then((array)=>{
-            
-            console.log('Both images are loaded');
-        })
-        .catch((err)=>{
-            console.log('Something went wrong')
-        })
-        
+    img.src = img1;
+});
+
+let promise2 = new Promise((resolve, reject)=>{
+    let img = document.createElement('img');
+    img.addEventListener('load', (ev) => {
+        // console.log(img);
+            resolve(img);
+        });
+    img.addEventListener('error', (err) => {
+            console.log('Image 2 is NOT loaded');
+            reject(err);
+        });
+    img.src = img2;
+});
+
+let promise3 = new Promise((resolve, reject)=>{
+    let img = document.createElement('img');
+    img.addEventListener('load', (ev) => {
+        // console.log(img);
+            resolve(img);
+        });
+    img.addEventListener('error', (err) => {
+            console.log('Image 3 is NOT loaded');
+            reject(err);
+        });
+    img.src = img3;
+});
+
+let promise4 = new Promise((resolve, reject)=>{
+    let img = document.createElement('img');
+    img.addEventListener('load', (ev) => {
+        // console.log(img);
+            resolve(img);
+        });
+    img.addEventListener('error', (err) => {
+            console.log('Image 4 is NOT loaded');
+            reject(err);
+        });
+    img.src = img4;
+});
+
+Promise.all([promise1, promise2, promise3, promise4]).then((array)=>{
+    console.log('ALL images are loaded');
+})
+.catch((err)=>{
+    console.log('Something went wrong')
+})
+
+
+// SLIDER
+
+var i = 0;
+var images = [];
+var time = 3000;
+    
+
+images[0] = img1;
+images[1] = img2;
+images[2] = img3;
+images[3] = img4;
+
+    function changeImg(){
+        console.log(images[i]);
+        document.slide.src = images[i];
+        if(i < images.length - 1){
+            i++; 
+        } else { 
+            i = 0;
+        }
+        setTimeout("changeImg()", time);
+    }
+    changeImg();
