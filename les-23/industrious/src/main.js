5ed510e4307450d9cfa3354f94e381af17f40b46
Ruben <rubenquaegebeur@gmail.Com>
2021-02-02 13:46:09 +0100

    Industrious

--- les-23/industrious/src/main.js
+++ les-23/industrious/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import '@/assets/css/font-awesome.min.css';
-import '@/assets/css/main.css';
+import "@/assets/css/font-awesome.min.css";
+import "@/assets/css/main.css";
+import router from "./router";
 
-createApp(App).mount("#app");
+createApp(App)
+  .use(router)
+  .mount("#app");
